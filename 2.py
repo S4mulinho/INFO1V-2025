d5f690bc30e2efddb7ae35e8f1cd069738512751
Debug-Sax <samuelpedro4830@gmail.com>
2025-09-10 17:57:38 -0300

    samu

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
-n1 = int(input("digite um numero:"))
-n2 = int(input("outro numero:"))
-Soma= n1 + n2
-print(Soma)
+n1 = int(input("digite um numero: "))
+n2 = int(input("outro numero: "))
+resultado = n1 + n2
+print(f"A soma de {n1} e {n2} é: {resultado}")
---
+++ 3.py
@@ -0,0 +1,11 @@
+al
+nota1, nota2, nota3, nota4: real
+mediafinal: real
+
+escreva("digite a p nota: ")
+leia(nota1)
+
+escreva("digite a s nota: ")
+leia(nota2)
+
+escreva
